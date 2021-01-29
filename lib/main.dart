f63dc8e35e7bcbd9679f475580b43c97919f62f3
Felix Reiniger <mc.nightfury34@gmail.com>
2021-01-29 16:06:00 +0100

    Implement tab system

--- lib/main.dart
+++ lib/main.dart
@@ -19,11 +19,39 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Text('1'),
-            Text('2')
-          ],
+        child: DefaultTabController(
+            length: 3,
+            child: Scaffold(
+              bottomNavigationBar: AppBar(
+                bottom: TabBar(
+                  indicatorColor: Colors.white,
+                  tabs: [
+                    Tab(text: "Status",),
+                    Tab(text: "Auswertung"),
+                    Tab(text: "Tipps")
+                  ],
+                ),
+              ),
+              body: TabBarView(
+                children: [
+                  Center(child: Text("I'll be a list!")),
+                  Center(child: ListView(
+                    children: <Widget>[
+                      Card(
+                        child: Text("Wie fühlst du dich heute?"),
+                      ),
+                      Card(
+                        child: Text("Warst du heute produktiv?"),
+                      ),
+                      Card(
+                        child: Text("Was hast du heute gemacht?"),
+                      )
+                    ],
+                  ),),
+                  Center(child: Text("Tipps"),)
+                ],
+              ),
+            )
         ),
       ),
     );
